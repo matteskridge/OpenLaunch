0b239cc6226c3bcc1d8071d9d1254f9f4658df5e
Matt Eskridge <matt@matteskridge.com>
2013-12-14 22:19:11 -0600

    Added support for comments and forums.

--- System/Solutions/OpenLaunchForum/Models/Forum.php
+++ System/Solutions/OpenLaunchForum/Models/Forum.php
@@ -11,4 +11,35 @@ class Forum extends Model {
 			"canview" => "list"
 		);
 	}
+
+	public function canView($person = null) {
+		return $this->can($person, "canview");
+	}
+
+	public function canPost($person = null) {
+		return $this->can($person, "canpost");
+	}
+
+	public function can($person, $mode) {
+		if ($person == null) {
+			if (Session::loggedIn()) {
+				$person = Session::getPerson();
+			} else {
+				return $this->get($mode) == array() || $this->get($mode) == "";
+			}
+		}
+
+		$view = $this->get($mode);
+		if ($view == array()) return true;
+
+		foreach ($person->getRoles() as $role) {
+			foreach ($view as $item) {
+				if ($item == $role->getId()) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 }
