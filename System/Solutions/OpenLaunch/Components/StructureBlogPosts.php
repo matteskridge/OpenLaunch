b6236a9ef34878ac73ea4c460c435e9b63016d6d
Matt Eskridge <matt@matteskridge.com>
2013-12-15 21:22:22 -0600

    You can now edit and delete blog posts

--- System/Solutions/OpenLaunch/Components/StructureBlogPosts.php
+++ System/Solutions/OpenLaunch/Components/StructureBlogPosts.php
@@ -6,6 +6,7 @@
 			<div class="blog-center-post-inner">
 				<div class="blog-center-post-options">
 					<a href="admin/index/structure/posts/<?php echo $item->getId() ?>/">Edit Post</a>
+					- <a href="admin/index/structure/posts/<?php echo $item->getId() ?>/delete/?sid=<?php echo session_id() ?>" onclick="return confirm('Are you sure you want to delete this post?')">Delete</a>
 				</div>
 				<h2><?php echo $item->get("name") ?></h2>
 				<?php echo Parser::parse($item->get("content")) ?>
--- System/Solutions/OpenLaunch/Components/StructurePostCompose.php
+++ System/Solutions/OpenLaunch/Components/StructurePostCompose.php
@@ -1,4 +1,5 @@
 <?php
+$id = (isset($post))?$post->getId():"0";
 $name = (isset($post))?$post->get("name"):"";
 $content = (isset($post))?$post->get("content"):"";
 $cat = (isset($post))?$post->get("page")->getId().",".$post->get("category")->getId():"";
@@ -7,22 +8,22 @@ $(document).ready(function() {
 	$(".blog-compose-main").height($(window).height()-180);
 });
 </script>
-<form action="admin/index/structure/posts/0/?sid=<?php echo session_id() ?>" method="post" class="blog-post-form">
+<form action="admin/index/structure/posts/<?php echo $id ?>/?sid=<?php echo session_id() ?>" method="post" class="blog-post-form">
 	<div class='blog-compose-settings'>
 		<div class="blog-compose-settings-inner">
 			Post Title:<input type='text' name='blogpost-name' value='<?php echo $name ?>' /><select name="blogpost-category">
 				<?php foreach (Page::findAll("Page", array("template" => "BlogPageType")) as $page) { ?>
 				<optgroup label="<?php echo $page->get("name") ?>">
 					<option value="<?php echo $page->getId() ?>,0"<?php if ($cat == $page->getId().",0") { ?> selected="yes"<?php } ?>>No Category</option>
-					<?php foreach (BlogCategory::findAll("BlogCategory", array("page" => $page)) as $cat) { ?>
-					<?php $cat2 = $page->getId().",".$cat->getId() ?>
-					<option value="<?php echo $cat2 ?>"<?php if ($cat == $cat2) { ?> selected="yes"<?php } ?>><?php echo $cat->get("name") ?></option>
+					<?php foreach (BlogCategory::findAll("BlogCategory", array("page" => $page)) as $c) { ?>
+					<?php $cat2 = $page->getId().",".$c->getId() ?>
+					<option value="<?php echo $cat2 ?>"<?php if ($cat == $cat2) { ?> selected="yes"<?php } ?>><?php echo $c->get("name") ?></option>
 					<?php } ?>
 				</optgroup>
 				<?php } ?>
 			</select>
 		</div>
 	</div><div class='blog-compose-main admin-full-editor'>
-		<?php echo Component::get("OpenLaunch.RichTextEditor", array("id" => "blogpost-text", "content" => Parser::parse($content))) ?>
+		<?php echo Component::get("OpenLaunch.RichTextEditor", array("id" => "blogpost-text", "value" => Parser::parse($content))) ?>
 	</div>
 </form>
