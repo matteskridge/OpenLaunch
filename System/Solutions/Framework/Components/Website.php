dbdfbf031e44e249f44b4853b694c3b0a8c3e1ae
Matt Eskridge <matt@matteskridge.com>
2013-12-15 18:14:12 -0600

    You can now upload a logo

--- System/Solutions/Framework/Components/Website.php
+++ System/Solutions/Framework/Components/Website.php
@@ -1,23 +1,27 @@
 <div class="website">
 	<div class="header-wrap">
 		<div class="header">
-			<div class="logo">
-				<?php echo Settings::getLogo() ?>
+			<div class="header-inner">
+				<div class="logo">
+					<a href="<?php echo Request::getBase() ?>"><?php echo Settings::getLogo() ?></a>
+				</div>
 			</div>
 		</div>
 		<div class="menu">
-			<div class="menu-right">
-				<?php if (Session::loggedIn()) { ?>
-					<div class="menu-item">
-						<a href="account/">Settings</a>
-					</div><div class="menu-item">
-						<a href="account/signout/">Sign Out</a>
-					</div>
-				<?php } else { ?>
-					<div class="menu-item"><a href="account/access/">Sign In / Register</a></div>
-				<?php } ?>
+			<div class="menu-inner">
+				<div class="menu-right">
+					<?php if (Session::loggedIn()) { ?>
+						<div class="menu-item">
+							<a href="account/">Settings</a>
+						</div><div class="menu-item">
+							<a href="account/signout/">Sign Out</a>
+						</div>
+					<?php } else { ?>
+						<div class="menu-item"><a href="account/access/">Sign In / Register</a></div>
+					<?php } ?>
+				</div>
+				<?php echo Component::get("*.TopMenu") ?>
 			</div>
-			<?php echo Component::get("*.TopMenu") ?>
 		</div>
 	</div>
     <?php if (Response::hasFlash()) { ?>
@@ -26,7 +30,9 @@
     </div>
     <?php } ?>
 	<div class="main">
-		<?php echo $content ?>
+		<div class="main-inner">
+			<?php echo $content ?>
+		</div>
 	</div>
 </div>
 
--- System/Solutions/OpenLaunch/SettingsItems/BrandingSettingsItem.php
+++ System/Solutions/OpenLaunch/SettingsItems/BrandingSettingsItem.php
@@ -16,7 +16,7 @@ class BrandingSettingsItem extends SettingsItem {
 		$form->add(new TextField("description", "Website Description"));
 		$form->add(new TextField("organization", "Organization Name"));
 		$form->add(new AttachmentField("logo", "Website Logo"));
-		$form->add(new FactorField("scale", "Logo Size", "1"));
+		$form->add(new FactorField("scale", "Logo Size", "0.2"));
 		$form->add(new MarginField("logoTop", "Margin Top", "0"));
 		$form->add(new MarginField("logoBottom", "Margin Bottom", "0"));
 		//$form->add(new TextField("link", "Install Directory"));
