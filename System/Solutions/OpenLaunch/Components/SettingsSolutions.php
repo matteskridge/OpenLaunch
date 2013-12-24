4f886e12f0107223fa177c95d701eb0bd0e73766
Matt Eskridge <matt@matteskridge.com>
2013-12-24 15:22:49 -0600

    Added metadata for bundles applications

--- System/Solutions/OpenLaunch/Components/SettingsSolutions.php
+++ System/Solutions/OpenLaunch/Components/SettingsSolutions.php
@@ -8,7 +8,10 @@
 			<div class="settings-role">
 				<div class="settings-role-inner">
 					<div class="settings-role-right">
-						This plugin cannot be deactivated
+						<?php if ($solution->isCore()) { ?>
+							This plugin cannot be deactivated -
+						<?php } ?>
+						<a href="<?php echo $solution->getAuthorWebsite() ?>"><?php echo $solution->getAuthor() ?></a>
 					</div>
                     <?php if ($solution->getName() == "") { ?>
                     <?php echo $solution->getFile()->getExtensionlessName() ?>
